89b3383229bf2c6d9926b2b39fc03e4987b7e576
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-06-19 15:46:46 -0400

    Add project files.

---
+++ MathMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me two numbers to perform some calculations!");
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+            Add(num1, num2);
+            
+        }
+        public static int Add(int num1, int num2)
+        {
+            var sum = num1 + num2;
+            Console.WriteLine($"Answer is:{ sum }");
+        }
+
+    }
+}
