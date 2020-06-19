098f7d82de4b04be9d397bac6da2a291af9f6ada
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-06-19 19:50:41 -0400

    added division and multiplication

--- MathMethods/Program.cs
+++ MathMethods/Program.cs
@@ -10,14 +10,32 @@ namespace MathMethods
             var num1 = int.Parse(Console.ReadLine());
             var num2 = int.Parse(Console.ReadLine());
             Add(num1, num2);
+            Multiply(num1, num2);
+            Divide(num1, num2);
             
         }
         public static int Add(int num1, int num2)
         {
             var sum = num1 + num2;
-            Console.WriteLine($"Here are your answers!{num1} + {num2} = {sum}");
+            Console.WriteLine($"Here are your answers! {num1} + {num2} = {sum}");
             return sum;
             
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            var sum = num1*num2;
+            Console.WriteLine($"Multiply {num1} * {num2} = {sum}");
+            return sum;
+
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            var sum = num1/num2;
+            Console.WriteLine($"Divide {num1} / {num2} = {sum}");
+            return sum;
+
         }
 
     }
